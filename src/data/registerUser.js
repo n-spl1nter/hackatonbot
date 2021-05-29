1247ca4f6fd7b75b41369adc31ec9f42ba90b61f
Alexey Rodin <razraz.odinodin@yandex.ru>
2021-05-29 13:28:20 +0300

    add dotenv

--- src/data/registerUser.js
+++ src/data/registerUser.js
@@ -4,8 +4,15 @@ import { sendMessage } from '../utils/api';
 export default async function registerUser(event) {
   const { sender: { id } } = event;
   await register(event);
-  const message = {
+  const greetings = {
     text: 'Привет 😊 . Я бот OmniFriend 👐 помогу тебе найти единомышленика среди сотрудников всей компании.',
   };
-  await sendMessage(message, id);
+  await sendMessage(greetings, id);
+  const about = {
+    text: `Возле окна ввода сообщения есть пункт меню:
+🔎 Поиск - умный алгорит поиска новых друзей и мероприятий
+💚 Совпадения - твои единомышленники
+⚙️ Настройки - можешь изменить настройки поиска`
+  };
+  await sendMessage(about, id);
 }
